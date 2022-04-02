8de0293f15dac1abb2d67be98201d80199d12b9e
Kourosh Delsouz <kdel2008@gmail.com>
2022-04-01 20:11:17 -0700

    Tetromino is dropping on an interval, collision works but needs bug fix

--- src/components/TetrisBoard/TetrisBoard.js
+++ src/components/TetrisBoard/TetrisBoard.js
@@ -1,103 +1,101 @@
 import React from 'react';
 import './TetrisBoard.scss';
-import createTetrominoes from '../Tetrominoes/Tetrominos';
+import createTetromino from '../Tetrominoes/Tetrominos';
 
 export default class TetrisBoard extends React.Component {
     state = {
         board: Array(20).fill(0).map(row => new Array(10).fill(0)),
-        count: 0,
+        isGameOver: false,
+        fallingTetromino: null,
+        tetrominoPosition: [0, 4],
     }
 
-    randomTetromino = () => {
+    createRandomTetromino = () => {
         const tetrominos = [
-            'T',
-            'O',
-            'L',
-            'J',
-            'I',
-            'S',
-            'Z',
+            't',
+            'o',
+            'l',
+            'j',
+            'i',
+            's',
+            'z',
         ];
-        return (tetrominos[Math.floor(Math.random() * tetrominos.length)]);
+        return createTetromino(tetrominos[Math.floor(Math.random() * tetrominos.length)]);
     }
 
     checkCollision = (board, tetromino, boardY, boardX) => {
-        const blockPositions = [];
+        const blockPositions = this.getBlockPositions(tetromino.grid, boardY, boardX);
 
-        for (let y = 0; y < tetromino.length; y++) {
-            for (let x = 0; x < tetromino[y].length; x++) {
-                if(tetromino[y][x] !== 0) {
-                    blockPositions.push([y + boardY, x + boardX])
-                }
-            }
-        }
         for (let i = 0; i < blockPositions.length; i++) {
-            const currentPosition = blockPositions[i] 
+            const currentPosition = blockPositions[i]
 
-            if(currentPosition[1] < 0) {
+            if (currentPosition[1] < 0) {
                 return true;
             }
-            if(currentPosition[1] > 9) {
+            if (currentPosition[1] > 9) {
                 return true;
             }
-            if(currentPosition[0] < 0) {
+            if (currentPosition[0] < 0) {
                 return true;
             }
-            if(currentPosition[0] > 19) {
+            if (currentPosition[0] > 19) {
                 return true;
             }
-            if(board[currentPosition[0]][currentPosition[1]] !== 0) {
+            if (board[currentPosition[0]][currentPosition[1]] !== 0) {
                 return true;
             }
         }
         return false;
     }
 
-    newBoard = () => {
-        const newBoard = this.state.board;
-        let newTetromino = createTetrominoes(this.randomTetromino())
+    mergeTetromino = (board, tetromino, boardY, boardX) => {
+        const blockPositions = this.getBlockPositions(tetromino.grid, boardY, boardX);
 
-        for (let y = 0; y < newTetromino.length; y++) {
-            for (let x = 0; x < newTetromino[y].length; x++) {
-                if (newTetromino[y][x]) {
-                    newBoard[y][x + 4] = newTetromino[y][x];
-                }
-            }
-        }
-        if (this.state.count > 17) {
-            this.setState({ board: newBoard, count: 0 });
-        } else {
-            this.setState({ board: newBoard });
+        for (let i = 0; i < blockPositions.length; i++) {
+            const currentPosition = blockPositions[i]
+
+            board[currentPosition[0]][currentPosition[1]] = tetromino.type
         }
     }
 
-    moveTetromino = () => {
-        const newBoard = Array(20).fill(0).map(row => new Array(10).fill(0));
-        // const newTetromino = createTetrominoes(this.randomTetromino())
+    getBlockPositions = (tetrominoGrid, boardY, boardX) => {
+        const blockPositions = [];
 
-        for (let y = 0; y < newBoard.length; y++) {
-            for (let x = 0; x < newBoard[y].length; x++) {
-                if (this.state.board[y][x] && y < newBoard.length - 1) {
-                    newBoard[y + 1][x] = this.state.board[y][x];
+        for (let y = 0; y < tetrominoGrid.length; y++) {
+            for (let x = 0; x < tetrominoGrid[y].length; x++) {
+                if (tetrominoGrid[y][x] !== 0) {
+                    blockPositions.push([y + boardY, x + boardX])
                 }
             }
         }
-        if (this.state.count > 17) {
-            this.newBoard();
-        } else {
-            this.setState({ board: newBoard, count: this.state.count + 1 });
+        return blockPositions;
+    }
+
+    moveTetromino = (board, tetromino, boardY, boardX) => {
+        const isColliding = this.checkCollision(board, tetromino, boardY + 1, boardX);
+
+        if (isColliding) {
+            const newBoard = this.state.board.map((arr) => { return arr.slice(); });
+            this.mergeTetromino(newBoard, tetromino, boardY + 1, boardX);
+            this.setState({ board: newBoard, tetrominoPosition: [boardY + 1, boardX] })
+            return;
         }
+
+        this.setState({ tetrominoPosition: [boardY + 1, boardX] })
+
+        console.log({ tetrominoPosition: [boardY + 1, boardX] })
     }
 
 
     componentDidMount = () => {
-        this.newBoard();
+        const newTetromino = this.createRandomTetromino()
+        const newBoard = this.state.board.map((arr) => { return arr.slice(); });
+
+        // this.mergeTetromino(newBoard, newTetromino, this.state.tetrominoPosition[0], this.state.tetrominoPosition[1])
+        this.setState({ fallingTetromino: newTetromino, board: newBoard })
         setInterval(() => {
-            this.moveTetromino();
+            this.moveTetromino(this.state.board, this.state.fallingTetromino, this.state.tetrominoPosition[0], this.state.tetrominoPosition[1]);
         }, 1000)
-        console.table(this.state.board)
-        console.log(this.state.board[0][5])
-        console.log(this.state.board[0][0])
     }
 
     componentDidUpdate = () => {
@@ -108,15 +106,19 @@ export default class TetrisBoard extends React.Component {
     }
 
     render = () => {
+        const boardWithTetromino = this.state.board.map((arr) => { return arr.slice(); });
+        if (this.state.fallingTetromino){
+            this.mergeTetromino(boardWithTetromino, this.state.fallingTetromino, this.state.tetrominoPosition[0], this.state.tetrominoPosition[1]);
+        } 
 
         return (
             <>
-                {this.state.board.map(row => {
+                {boardWithTetromino.map((row, i) => {
                     return (
-                        <div className="row">
-                            {row.map(cell => {
+                        <div key={i} className="row">
+                            {row.map((cell, j) => {
                                 return (
-                                    <div className={(cell === 0 ? 'cell' : `cell cell__${cell}`)}>
+                                    <div key={j} className={(cell === 0 ? 'cell' : `cell cell__${cell}`)}>
                                         {/* <div className="cell_sparkle"></div> */}
                                     </div>
                                 )
--- src/components/Tetrominoes/Tetrominos.js
+++ src/components/Tetrominoes/Tetrominos.js
@@ -1,53 +1,75 @@
-export default function createTetrominoes(type) {
-    
-    if (type === 'T') {
-        return [
-            ['t', 't', 't'],
-            [0, 't', 0],
-            [0, 0, 0],
-        ];
+export default function createTetromino(type) {
 
-    } else if (type === 'O') {
-        return [
-            ['o', 'o'],
-            ['o', 'o'],
-        ];
+    if (type === 't') {
+        return {
+            grid: [
+                ['t', 't', 't'],
+                [0, 't', 0],
+                [0, 0, 0],
+            ],
+            type: type
+        };
 
-    } else if (type === 'L') {
-        return [
-            [0, 'l', 0],
-            [0, 'l', 0],
-            [0, 'l', 'l'],
-        ];
+    } else if (type === 'o') {
+        return {
+            grid: [
+                ['o', 'o'],
+                ['o', 'o'],
+            ],
+            type: type
+        };
 
-    } else if (type === 'J') {
-        return [
-            [0, 'j', 0],
-            [0, 'j', 0],
-            ['j', 'j', 0],
-        ];
+    } else if (type === 'l') {
+        return {
+            grid: [
+                [0, 'l', 0],
+                [0, 'l', 0],
+                [0, 'l', 'l'],
+            ],
+            type: type
+        };
 
-    } else if (type === 'I') {
-        return [
-            [0, 'i', 0, 0],
-            [0, 'i', 0, 0],
-            [0, 'i', 0, 0],
-            [0, 'i', 0, 0],
-        ];
+    } else if (type === 'j') {
+        return {
+            grid: [
+                [0, 'j', 0],
+                [0, 'j', 0],
+                ['j', 'j', 0],
+            ],
+            type: type
+        };
 
-    } else if (type === 'S') {
-        return [
-            [0, 's', 's'],
-            ['s', 's', 0],
-            [0, 0, 0],
-        ];
-        
-    } else if (type === 'Z') {
-        return [
-            ['z', 'z', 0],
-            [0, 'z', 'z'],
-            [0, 0, 0],
-        ];
+
+    } else if (type === 'i') {
+        return {
+            grid: [
+                [0, 'i', 0, 0],
+                [0, 'i', 0, 0],
+                [0, 'i', 0, 0],
+                [0, 'i', 0, 0],
+            ],
+            type: type
+        };
+
+    } else if (type === 's') {
+        return {
+            grid: [
+                [0, 's', 's'],
+                ['s', 's', 0],
+                [0, 0, 0],
+            ],
+            type: type
+        };
+
+    } else if (type === 'z') {
+        return {
+            grid: [
+                ['z', 'z', 0],
+                [0, 'z', 'z'],
+                [0, 0, 0],
+            ],
+            type: type
+        };
     }
-    return createTetrominoes;
+    return createTetromino;
 }
