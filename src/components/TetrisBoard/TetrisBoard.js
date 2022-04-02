16e3a893222b9ad5f9b46e733cc84e26265977eb
Kourosh Delsouz <kdel2008@gmail.com>
2022-04-01 18:34:36 -0700

    added a collision check function

--- src/components/TetrisBoard/TetrisBoard.js
+++ src/components/TetrisBoard/TetrisBoard.js
@@ -18,51 +18,86 @@ export default class TetrisBoard extends React.Component {
             'S',
             'Z',
         ];
-        return(tetrominos[Math.floor(Math.random() * tetrominos.length)]);
+        return (tetrominos[Math.floor(Math.random() * tetrominos.length)]);
+    }
+
+    checkCollision = (board, tetromino, boardY, boardX) => {
+        const blockPositions = [];
+
+        for (let y = 0; y < tetromino.length; y++) {
+            for (let x = 0; x < tetromino[y].length; x++) {
+                if(tetromino[y][x] !== 0) {
+                    blockPositions.push([y + boardY, x + boardX])
+                }
+            }
+        }
+        for (let i = 0; i < blockPositions.length; i++) {
+            const currentPosition = blockPositions[i] 
+
+            if(currentPosition[1] < 0) {
+                return true;
+            }
+            if(currentPosition[1] > 9) {
+                return true;
+            }
+            if(currentPosition[0] < 0) {
+                return true;
+            }
+            if(currentPosition[0] > 19) {
+                return true;
+            }
+            if(board[currentPosition[0]][currentPosition[1]] !== 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     newBoard = () => {
         const newBoard = this.state.board;
         let newTetromino = createTetrominoes(this.randomTetromino())
 
-        for(let y = 0; y < newTetromino.length; y++) {
-            for(let x = 0; x < newTetromino[y].length; x++) {
+        for (let y = 0; y < newTetromino.length; y++) {
+            for (let x = 0; x < newTetromino[y].length; x++) {
                 if (newTetromino[y][x]) {
                     newBoard[y][x + 4] = newTetromino[y][x];
                 }
             }
         }
-        if(this.state.count > 17 ) {
-            this.setState({board: newBoard, count: 0});
+        if (this.state.count > 17) {
+            this.setState({ board: newBoard, count: 0 });
         } else {
-            this.setState({board: newBoard});
+            this.setState({ board: newBoard });
         }
     }
 
-    moveBoard = () => {
+    moveTetromino = () => {
         const newBoard = Array(20).fill(0).map(row => new Array(10).fill(0));
         // const newTetromino = createTetrominoes(this.randomTetromino())
 
-        for(let y = 0; y < newBoard.length; y++) {
-            for(let x = 0; x < newBoard[y].length; x++) {
+        for (let y = 0; y < newBoard.length; y++) {
+            for (let x = 0; x < newBoard[y].length; x++) {
                 if (this.state.board[y][x] && y < newBoard.length - 1) {
-                    newBoard[y+1][x] = this.state.board[y][x];
+                    newBoard[y + 1][x] = this.state.board[y][x];
                 }
             }
         }
-        if(this.state.count > 17 ) {
+        if (this.state.count > 17) {
             this.newBoard();
         } else {
-            this.setState({board: newBoard, count: this.state.count+ 1});
+            this.setState({ board: newBoard, count: this.state.count + 1 });
         }
     }
 
 
     componentDidMount = () => {
         this.newBoard();
-        setInterval(()=> {
-            this.moveBoard();
-        },500)
+        setInterval(() => {
+            this.moveTetromino();
+        }, 1000)
+        console.table(this.state.board)
+        console.log(this.state.board[0][5])
+        console.log(this.state.board[0][0])
     }
 
     componentDidUpdate = () => {
@@ -73,7 +108,7 @@ export default class TetrisBoard extends React.Component {
     }
 
     render = () => {
-        console.table(this.state.board)
+
         return (
             <>
                 {this.state.board.map(row => {
