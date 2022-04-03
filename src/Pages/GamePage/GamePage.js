fcf1169caab943e825cb52f9500dcd6a24834dae
Kourosh Delsouz <kdel2008@gmail.com>
2022-04-03 16:06:16 -0700

    implemented backend to front end

--- src/Pages/GamePage/GamePage.js
+++ src/Pages/GamePage/GamePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './GamePage.scss';
 import TetrisBoard from '../../components/TetrisBoard/TetrisBoard';
 import GameScore from '../../components/GameScore/GameScore';
@@ -12,11 +13,28 @@ export default class GamePage extends React.Component {
         showGameOverModal: false,
         showPausedModal: false,
         gameScore: 0,
+        leaderboard: [],
         nextTetrominoesPreview: [
             createRandomTetromino(),
             createRandomTetromino(),
             createRandomTetromino()
         ],
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5050/scores')
+            .then((res) => {
+                let top10Scores = res.data.sort((scoreObj1, scoreObj2) => {
+                    return scoreObj2.score - scoreObj1.score;
+                })
+                if (top10Scores.length > 10) {
+                    top10Scores = top10Scores.slice(0, 10)
+                }
+                this.setState({ leaderboard: top10Scores })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     toggleShowGameOverModal = () => {
@@ -38,7 +56,7 @@ export default class GamePage extends React.Component {
     grabNextTetromino = () => {
         let copiedTetrominoesPreview = JSON.parse(JSON.stringify(this.state.nextTetrominoesPreview))
         const nextTetromino = copiedTetrominoesPreview.shift();
-        
+
         copiedTetrominoesPreview.push(createRandomTetromino());
         this.setState({ nextTetrominoesPreview: copiedTetrominoesPreview })
         return nextTetromino;
@@ -62,9 +80,9 @@ export default class GamePage extends React.Component {
                 <div className="game-page">
                     <div>
                         <GameScore gameScore={this.state.gameScore} />
-                        <Leaderboard />
+                        <Leaderboard leaderboard={this.state.leaderboard} />
                     </div>
-                    <TetrisBoard createAndSetPreviewTetrominoes={this.createAndSetPreviewTetrominoes} resetScore={this.resetScore} toggleShowGamePausedModal={this.toggleShowGamePausedModal} grabNextTetromino={this.grabNextTetromino} addPoints={this.addPointsToScore} showGameOverModal={this.state.showGameOverModal} createRandomTetromino={this.createRandomTetromino} toggleShowGameOverModal={this.toggleShowGameOverModal} showPausedModal={this.state.showPausedModal}  tetrominoesPreview={this.state.tetrominoesPreview} />
+                    <TetrisBoard createAndSetPreviewTetrominoes={this.createAndSetPreviewTetrominoes} resetScore={this.resetScore} toggleShowGamePausedModal={this.toggleShowGamePausedModal} grabNextTetromino={this.grabNextTetromino} addPoints={this.addPointsToScore} showGameOverModal={this.state.showGameOverModal} createRandomTetromino={this.createRandomTetromino} toggleShowGameOverModal={this.toggleShowGameOverModal} showPausedModal={this.state.showPausedModal} tetrominoesPreview={this.state.tetrominoesPreview} />
                     <PreviewTetrominoes nextTetrominoesPreview={this.state.nextTetrominoesPreview} createRandomTetromino={this.createRandomTetromino} tetrominoesPreview={this.state.nextTetrominoesPreview} toggleShowGamePausedModal={this.toggleShowGamePausedModal} />
                 </div>
             </>
--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,53 +1,7 @@
 import React from 'react';
 import './Leaderboard.scss';
 
-const mockLeaderBoard = [
-    
-    {
-        name: 'player',
-        score: 9999
-    },
-    {
-        name: 'player2',
-        score: 9999
-    },
-    {
-        name: 'player3',
-        score: 9999
-    },
-    {
-        name: 'player4',
-        score: 9999
-    },
-    {
-        name: 'player5',
-        score: 9999
-    },
-    {
-        name: 'player6',
-        score: 9999
-    },
-    {
-        name: 'player7',
-        score: 9999
-    },
-    {
-        name: 'player8',
-        score: 9999
-    },
-    {
-        name: 'player9',
-        score: 9999
-    },
-    {
-        name: 'player10',
-        score: 9999
-    }
-]
-
-
-
-export default function Leaderboard() {
+export default function Leaderboard(props) {
 
     return (
         <section className="leaderboard">
@@ -55,7 +9,7 @@ export default function Leaderboard() {
                     <h2>Leaderboard</h2>
                 </div>
             <ul className="leaderboard__list">
-                {mockLeaderBoard.map((obj, i) => {
+                {props.leaderboard.map((obj, i) => {
                     return (
                         <li key={i} className="leaderboard__list-item">
                             <h3>{i + 1}</h3>
